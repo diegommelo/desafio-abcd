e407d1d370f218260be9fa7bcd42a5b7269079aa
Diego Melo <d.melo@coblue.com.br>
2021-09-12 14:28:36 -0300

    core: adiciona visual menu lateral no desktop

--- src/components/Elements/MenuLateral.vue
+++ src/components/Elements/MenuLateral.vue
@@ -2,19 +2,37 @@
   <div class="menu-lateral">
     <Logo size="small"/>
     <ul>
-      <li>
-        <font-awesome-icon icon="user" />
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
+      <router-link to="#">
+        <li>
+          <font-awesome-icon icon="user-circle" size="lg" class="menu-lateral-icon" />
+          Perfil dos Alunos
+        </li>
+      </router-link>
+      <router-link to="#">
+        <li>
+          <font-awesome-icon icon="file-signature" size="lg" class="menu-lateral-icon" />
+          Prova de Português
+        </li>
+      </router-link>
+      <router-link to="#">
+        <li>
+          <font-awesome-icon icon="desktop" size="lg" class="menu-lateral-icon" />
+          Atividades Digitais
+        </li>
+      </router-link>
+      <router-link to="#">
+        <li>
+          <font-awesome-icon icon="question-circle" size="lg" class="menu-lateral-icon" />
+          Ajuda
+        </li>
+      </router-link>
+      <router-link to="#">
+        <li>
+          <font-awesome-icon icon="cog" size="lg" class="menu-lateral-icon" />
+          Configurações
+        </li>
+      </router-link>
+
     </ul>
   </div>
 </template>
@@ -33,16 +51,35 @@ export default {
 <style lang="scss">
 @import '../../scss/_variables.scss';
 
-ul {
+.menu-lateral-icon {
+  color: $text-color;
+  opacity: 0.7;
+  padding-right: 20px;
+}
+  .menu-lateral {
+    width: 20vw;
+    background-color: $white;
+    margin-top: 40px;
+  }
+.menu-lateral li {
+  color: $text-color;
+  font-weight: bold;
+  opacity: 0.7;
+  transition: opacity 0.2s ease-in-out;
+  padding: 20px;
+  font-size: 1.2rem;
+  margin: 30px 0;
+}
+  .menu-lateral li:hover {
+    opacity: 1;
+    background-color: $primary;
+    color: $white;
+  }
+
+.menu-lateral ul {
   list-style: none;
   padding: 0;
+  margin-top: 40px;
 }
-li {
-  padding: 10px;
-  border-bottom: 1px solid #ccc;
-}
-.menu-lateral {
-  width: 20vw;
-  background-color: $white;
-}
+
 </style>
